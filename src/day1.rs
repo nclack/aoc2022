9074bbec7a9bf8fc5e17f52d0b110253c3aa85e5
Nathan Clack <nclack@chanzuckerberg.com>
2022-12-01 17:26:24 -0800

    day1

---
+++ src/day1.rs
@@ -0,0 +1,48 @@
+use nom::{
+    character::complete::{char, digit1},
+    combinator::{map_res, opt},
+    multi::many1,
+    sequence::terminated,
+    IResult,
+};
+
+fn parse(input: &str) -> IResult<&str, Vec<Vec<u32>>> {
+    let snacks = many1(map_res(terminated(digit1, opt(char('\n'))), |src| {
+        u32::from_str_radix(src, 10)
+    }));
+    let mut elves = many1(terminated(snacks, opt(char('\n'))));
+    elves(input)
+}
+
+pub fn part1(input: &str) -> u32 {
+    parse(input)
+        .unwrap()
+        .1
+        .iter()
+        .map(|elf| elf.iter().sum::<u32>())
+        .max()
+        .unwrap()
+}
+
+pub fn part2(input: &str) -> u32 {
+    let mut elves: Vec<_> = parse(input)
+        .unwrap()
+        .1
+        .iter()
+        .map(|elf| elf.iter().sum::<u32>())
+        .collect();
+    elves.sort();
+    elves.iter().rev().take(3).sum()
+}
+
+#[test]
+fn day1() {
+    assert_eq!(
+        24000,
+        part1(include_str!("../assets/day1.test.txt"))
+    );
+    assert_eq!(
+        45000,
+        part2(include_str!("../assets/day1.test.txt"))
+    );
+}
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+mod day1;
+
+macro_rules! problems {
+    ()=>{};
+    ($day:tt $($parts:ident)+, $($rest:tt)*) => {
+        problems!($day $($parts)+);
+        problems!($($rest)*);
+    };
+    ($day:tt $part:tt $($rest:tt)+) => {
+        problems!($day $part);
+        problems!($day $($rest)+);
+    };
+    ($day:tt $part:tt)=>{
+        println!(
+            "{} {}\t{:?}",
+            stringify!($day),
+            stringify!($part),
+            $day::$part(include_str!(concat!("../assets/",stringify!($day),".txt")))
+        );
+    };
+}
+
+fn main() {
+    problems!(
+        day1 part1 part2,
+    );
+}
