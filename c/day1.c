b9015e977d2db66852d0f0dceff3a60851e6b571
Nathan Clack <nclack@chanzuckerberg.com>
2022-12-02 00:52:29 -0800

    day1 parser combinators in c

--- c/day1.c
+++ c/day1.c
@@ -12,14 +12,14 @@ struct slice {
   char *beg, *end;
 };
 
-enum parse_kind { pk_single, pk_many, pk_u32, pk_error };
+enum parse_kind { pk_slice, pk_list, pk_u32, pk_error };
 
 struct parse {
   struct parse_value {
     enum parse_kind kind;
     union {
-      struct slice match;
-      struct match_list_item *matches;
+      struct slice slice;
+      struct list_item *list;
       uint32_t u32;
     };
   } value;
@@ -27,9 +27,9 @@ struct parse {
   int is_match;
 };
 
-struct match_list_item {
+struct list_item {
   struct parse_value value;
-  struct match_list_item *next;
+  struct list_item *next;
 };
 
 struct parser {
@@ -64,8 +64,8 @@ static struct parse digits_(struct parser *unused_, struct slice input) {
   return (struct parse){
       .value =
           {
-              .kind = pk_single,
-              .match = (struct slice){.beg = input.beg, .end = c},
+              .kind = pk_slice,
+              .slice = (struct slice){.beg = input.beg, .end = c},
           },
       .rest = (struct slice){.beg = c, .end = input.end},
       .is_match = (input.beg < c),
@@ -94,8 +94,8 @@ static struct parse onechar_(struct parser *ctx_, struct slice input) {
   return (struct parse){
       .value =
           {
-              .kind = pk_single,
-              .match = (struct slice){.beg = input.beg, .end = c},
+              .kind = pk_slice,
+              .slice = (struct slice){.beg = input.beg, .end = c},
           },
       .rest = (struct slice){.beg = c, .end = input.end},
       .is_match = (input.beg < c),
@@ -127,8 +127,8 @@ static struct parse terminated_(struct parser *ctx_, struct slice input) {
   return (struct parse){
       .value =
           {
-              .kind = pk_single,
-              .match = (struct slice){.beg = input.beg, .end = input.beg},
+              .kind = pk_slice,
+              .slice = (struct slice){.beg = input.beg, .end = input.beg},
           },
       .rest = input,
       .is_match = 0,
@@ -176,13 +176,13 @@ struct many1_s {
 static struct parse many1_(struct parser *ctx_, struct slice input) {
   struct many1_s *ctx = containerof(struct many1_s, ctx_, parser);
   struct parse a = {.rest = input};
-  struct match_list_item *list = 0, *tail = 0;
+  struct list_item *list = 0, *tail = 0;
   do {
     a = eval(ctx->inner, a.rest);
     if (a.is_match) {
       // add match to head
-      struct match_list_item *node = malloc(sizeof(*node));
-      *node = (struct match_list_item){.value = a.value, .next = 0};
+      struct list_item *node = malloc(sizeof(*node));
+      *node = (struct list_item){.value = a.value, .next = 0};
       if (!list)
         list = node;
       if (tail)
@@ -193,8 +193,8 @@ static struct parse many1_(struct parser *ctx_, struct slice input) {
   return (struct parse){
       .value =
           {
-              .kind = pk_many,
-              .matches = list,
+              .kind = pk_list,
+              .list = list,
           },
       .rest = a.rest,
       .is_match = (list != 0),
@@ -221,24 +221,26 @@ struct map_u32_s {
 static struct parse_value apply_fu32(uint32_t (*func)(struct slice *s),
                                      struct parse_value *v) {
   switch (v->kind) {
-  case pk_single: {
-    return (struct parse_value){.kind = pk_u32, .u32 = func(&v->match)};
+  case pk_slice: {
+    return (struct parse_value){.kind = pk_u32, .u32 = func(&v->slice)};
   }
 
-  case pk_many: {
-    struct match_list_item *list=0,*tail=0;
-    for(struct match_list_item *cur=v->matches;cur;cur=cur->next) {
-      struct match_list_item *buf = malloc(sizeof(*buf));
-      buf->value=apply_fu32(func,&cur->value);
-      if(!list) list=buf;
-      if(tail) tail->next=buf;
-      tail=buf;
+  case pk_list: {
+    struct list_item *list = 0, *tail = 0;
+    for (struct list_item *cur = v->list; cur; cur = cur->next) {
+      struct list_item *buf = malloc(sizeof(*buf));
+      buf->value = apply_fu32(func, &cur->value);
+      if (!list)
+        list = buf;
+      if (tail)
+        tail->next = buf;
+      tail = buf;
     }
-    return (struct parse_value){.kind = pk_many, .matches = list};
+    return (struct parse_value){.kind = pk_list, .list = list};
   }
 
   default:
-    return (struct parse_value){.kind=pk_error};
+    return (struct parse_value){.kind = pk_error};
   }
 }
 
@@ -246,14 +248,15 @@ static struct parse map_u32_(struct parser *ctx_, struct slice input) {
   struct map_u32_s *ctx = containerof(struct map_u32_s, ctx_, parser);
   struct parse a = eval(ctx->inner, input);
   if (a.is_match) {
-    return (struct parse) {
-      .value=apply_fu32(ctx->func,&a.value),
-      .rest=a.rest,
-      .is_match=1
-    };
+    return (struct parse){.value = apply_fu32(ctx->func, &a.value),
+                          .rest = a.rest,
+                          .is_match = 1};
   }
   return (struct parse){
-    .value={.kind = pk_error,},
+      .value =
+          {
+              .kind = pk_error,
+          },
       .is_match = 0,
       .rest = input,
   };
@@ -266,6 +269,62 @@ static struct parser *map_u32(struct parser *target,
       .parser.proc = map_u32_,
       .inner = target,
       .func = func,
+  };
+  return &ctx->parser;
+}
+
+// sum_u32
+
+struct sum_u32_s {
+  struct parser parser;
+  struct parser *inner;
+};
+
+static struct parse_value apply_sum_u32(struct parse_value *v) {
+  switch (v->kind) {
+
+  case pk_u32:
+    return (struct parse_value){.kind=pk_u32, .u32=v->u32};
+
+  case pk_list: {
+    uint32_t acc=0;
+    for(struct list_item *cur=v->list;cur;cur=cur->next) {
+      struct parse_value t=apply_sum_u32(&cur->value);
+      if(t.kind!=pk_u32)
+        return (struct parse_value){.kind=pk_error};  
+      acc+=t.u32;
+    }
+    return (struct parse_value){.kind=pk_u32, .u32=acc};
+  }
+
+  default:
+    return (struct parse_value){.kind=pk_error};
+  };
+}
+
+static struct parse sum_u32_(struct parser *ctx_, struct slice input) {
+  struct sum_u32_s *ctx = containerof(struct sum_u32_s, ctx_, parser);
+  struct parse a = eval(ctx->inner, input);
+  if (a.is_match) {
+    return (struct parse){
+        .value = apply_sum_u32(&a.value), .rest = a.rest, .is_match = 1};
+  } else {
+    return (struct parse){
+        .value =
+            {
+                .kind = pk_error,
+            },
+        .is_match = 0,
+        .rest = input,
+    };
+  }
+}
+
+static struct parser *sum_u32(struct parser *target) {
+  struct sum_u32_s *ctx = malloc(sizeof(*ctx));
+  *ctx = (struct sum_u32_s){
+      .parser.proc = sum_u32_,
+      .inner = target,
   };
   return &ctx->parser;
 }
@@ -292,11 +351,49 @@ static struct slice input() {
   return (struct slice){.beg = buf, .end = buf + s.st_size};
 }
 
+static int cmp(const uint32_t* a,const uint32_t*b) {
+  return (*a<*b)?1:((*a>*b)?-1:0);
+}
+
 int main() {
-  pslice(eval(terminated(
-    map_u32(many1(terminated(digits(), opt(onechar('\n')))), as_u32), 
-    opt(onechar('\n'))),
-              input())
-             .rest);
+  struct parser
+    *snack=map_u32(terminated(digits(), opt(onechar('\n'))), as_u32),
+    *elf=terminated(sum_u32(many1(snack)), opt(onechar('\n')));
+  struct parse result=eval(many1(elf),input());
+
+  if( !(result.value.kind==pk_list && result.value.list && result.value.list->value.kind==pk_u32) ) {
+    printf("Expected a list of u32");
+    return 1;
+  }
+
+  // part 1, find the max
+  {
+    uint32_t mx=0;
+    
+    for(struct list_item *cur=result.value.list;cur;cur=cur->next) {
+      uint32_t v=cur->value.u32;
+      mx=v>mx?v:mx;
+    }
+
+    printf("part 1 - %u\n",mx);
+  }
+
+  // part 2
+  {
+    int n=0;
+    for(struct list_item *cur=result.value.list;cur;cur=cur->next) 
+      ++n;
+
+    uint32_t 
+      *buf=malloc(n*sizeof(*buf)),
+      *v=buf;
+
+    for(struct list_item *cur=result.value.list;cur;cur=cur->next,++v) 
+      *v=cur->value.u32;
+
+    qsort(buf,n,sizeof(*buf),(int(*)(const void*,const void*))cmp);
+    printf("part 2 - %u\n",buf[0]+buf[1]+buf[2]); 
+  }
+
   return 0;
 }
